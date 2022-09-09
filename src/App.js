4b57141185611e530b7825d2e153cd729048774c
Lucia Rondan <luciabrondan@gmail.com>
2022-09-09 00:31:55 -0300

    contador

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListcontainer from './components/ItemListContainer';
 
 function App() {
+  const [counter, setCounter] = useState(0) 
+  const handleClickSuma =() => {
+  setCounter (counter + 1);
+  }
+  const handleClickResta = () =>{
+    setCounter (counter - 1)
+  }
+
   return (
     <div className='container'>
        <NavBar></NavBar>
        <ItemListcontainer greeting={'Saludos'}></ItemListcontainer>
+       <p>Contador:{counter}</p>
+       <button onClick= {handleClickSuma} className= 'btn'> + </button>
+       <button onClick= {handleClickResta} className= 'btn'> - </button>
     </div>
+    
   );
 }
 
